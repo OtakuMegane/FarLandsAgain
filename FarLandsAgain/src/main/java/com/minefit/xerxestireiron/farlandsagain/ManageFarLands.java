a0d6afed665aa4d5aeb09c5de70e28c4a8b04588
OtakuMegane <otakumegane@insaneotaku.com>
2018-05-07 14:26:53 -0400

    1.9.4 support back in

--- FarLandsAgain/src/main/java/com/minefit/xerxestireiron/farlandsagain/ManageFarLands.java
+++ FarLandsAgain/src/main/java/com/minefit/xerxestireiron/farlandsagain/ManageFarLands.java
@@ -7,6 +7,7 @@ public class ManageFarLands {
 
     private final FarLandsAgain plugin;
     private final World world;
+    private com.minefit.xerxestireiron.farlandsagain.v1_9_R2.LoadFarlands LF9R2;
     private com.minefit.xerxestireiron.farlandsagain.v1_10_R1.LoadFarlands LF10R1;
     private com.minefit.xerxestireiron.farlandsagain.v1_11_R1.LoadFarlands LF11R1;
     private com.minefit.xerxestireiron.farlandsagain.v1_12_R1.LoadFarlands LF12R1;
@@ -14,21 +15,30 @@ public class ManageFarLands {
     public ManageFarLands(World world, FarLandsAgain instance) {
         this.plugin = instance;
         this.world = world;
-        ConfigurationSection worldConfig = this.plugin.getConfig().getConfigurationSection("worlds." + this.world.getName());
+        ConfigurationSection worldConfig = this.plugin.getConfig()
+                .getConfigurationSection("worlds." + this.world.getName());
 
-        if (this.plugin.version.equals("v1_10_R1")) {
-            this.LF10R1 = new com.minefit.xerxestireiron.farlandsagain.v1_10_R1.LoadFarlands(this.world, worldConfig, this.plugin.getName());
+        if (this.plugin.version.equals("v1_9_R2")) {
+            this.LF9R2 = new com.minefit.xerxestireiron.farlandsagain.v1_9_R2.LoadFarlands(this.world, worldConfig,
+                    this.plugin.getName());
+        } else if (this.plugin.version.equals("v1_10_R1")) {
+            this.LF10R1 = new com.minefit.xerxestireiron.farlandsagain.v1_10_R1.LoadFarlands(this.world, worldConfig,
+                    this.plugin.getName());
         } else if (this.plugin.version.equals("v1_11_R1")) {
-            this.LF11R1 = new com.minefit.xerxestireiron.farlandsagain.v1_11_R1.LoadFarlands(this.world, worldConfig, this.plugin.getName());
+            this.LF11R1 = new com.minefit.xerxestireiron.farlandsagain.v1_11_R1.LoadFarlands(this.world, worldConfig,
+                    this.plugin.getName());
         } else if (this.plugin.version.equals("v1_12_R1")) {
-            this.LF12R1 = new com.minefit.xerxestireiron.farlandsagain.v1_12_R1.LoadFarlands(this.world, worldConfig, this.plugin.getName());
+            this.LF12R1 = new com.minefit.xerxestireiron.farlandsagain.v1_12_R1.LoadFarlands(this.world, worldConfig,
+                    this.plugin.getName());
         }
     }
 
     // Always good to clean up when disabling a plugin
     // Especially if it's a /reload command
     public void restoreGenerator() {
-if (this.plugin.version.equals("v1_10_R1")) {
+        if (this.plugin.version.equals("v1_9_R2")) {
+            this.LF9R2.restoreGenerator();
+        } else if (this.plugin.version.equals("v1_10_R1")) {
             this.LF10R1.restoreGenerator();
         } else if (this.plugin.version.equals("v1_11_R1")) {
             this.LF11R1.restoreGenerator();
---
+++ v1_9_R2/src/main/java/com/minefit/xerxestireiron/farlandsagain/v1_9_R2/LoadFarlands.java
@@ -0,0 +1,110 @@
+package com.minefit.xerxestireiron.farlandsagain.v1_9_R2;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import org.bukkit.World;
+import org.bukkit.World.Environment;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.craftbukkit.v1_9_R2.CraftWorld;
+
+import com.minefit.xerxestireiron.farlandsagain.Messages;
+import com.minefit.xerxestireiron.farlandsagain.v1_9_R2.FLAChunkProviderGenerate;
+import com.minefit.xerxestireiron.farlandsagain.v1_9_R2.FLAChunkProviderHell;
+import com.minefit.xerxestireiron.farlandsagain.v1_9_R2.FLAChunkProviderTheEnd;
+
+import net.minecraft.server.v1_9_R2.ChunkGenerator;
+import net.minecraft.server.v1_9_R2.WorldServer;
+
+public class LoadFarlands {
+    private final World world;
+    private final WorldServer nmsWorld;
+    private final Messages messages;
+    private ChunkGenerator originalGenerator;
+    private final ConfigurationSection worldConfig;
+
+    public LoadFarlands(World world, ConfigurationSection worldConfig, String pluginName) {
+        this.world = world;
+        this.worldConfig = worldConfig;
+        this.nmsWorld = ((CraftWorld) world).getHandle();
+        this.messages = new Messages(pluginName);
+        overrideGenerator();
+    }
+
+    public void restoreGenerator() {
+        try {
+            Field cp = net.minecraft.server.v1_9_R2.ChunkProviderServer.class.getDeclaredField("chunkGenerator");
+            cp.setAccessible(true);
+            setFinal(cp, this.originalGenerator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void overrideGenerator() {
+        String worldName = this.world.getName();
+        this.originalGenerator = this.nmsWorld.getChunkProviderServer().chunkGenerator;
+        String originalGenName = this.originalGenerator.getClass().getSimpleName();
+        boolean genFeatures = this.nmsWorld.getWorldData().shouldGenerateMapFeatures();
+        long worldSeed = this.nmsWorld.getSeed();
+        Environment environment = this.world.getEnvironment();
+        String genOptions = this.nmsWorld.getWorldData().getGeneratorOptions();
+
+        if (originalGenName.equals("FLAChunkProviderGenerate") || originalGenName.equals("FLAChunkProviderHell")
+                || originalGenName.equals("FLAChunkProviderTheEnd")) {
+            this.messages.alreadyEnabled(worldName);
+            return;
+        }
+
+        try {
+            Field cp = net.minecraft.server.v1_9_R2.ChunkProviderServer.class.getDeclaredField("chunkGenerator");
+            cp.setAccessible(true);
+
+            if (environment == Environment.NORMAL) {
+                if (!originalGenName.equals("NormalChunkGenerator") && !originalGenName.equals("TimedChunkGenerator")) {
+                    this.messages.unknownGenerator(worldName, originalGenName);
+                    return;
+                }
+
+                FLAChunkProviderGenerate generator = new FLAChunkProviderGenerate(this.nmsWorld, worldSeed, genFeatures,
+                        genOptions, worldConfig);
+                setFinal(cp, generator);
+            } else if (environment == Environment.NETHER) {
+                if (!originalGenName.equals("NetherChunkGenerator") && !originalGenName.equals("TimedChunkGenerator")) {
+                    this.messages.unknownGenerator(worldName, originalGenName);
+                    return;
+                }
+
+                FLAChunkProviderHell generator = new FLAChunkProviderHell(this.nmsWorld, genFeatures, worldSeed,
+                        worldConfig);
+                setFinal(cp, generator);
+            } else if (environment == Environment.THE_END) {
+                if (!originalGenName.equals("SkyLandsChunkGenerator")
+                        && !originalGenName.equals("TimedChunkGenerator")) {
+                    this.messages.unknownGenerator(worldName, originalGenName);
+                    return;
+                }
+
+                FLAChunkProviderTheEnd generator = new FLAChunkProviderTheEnd(this.nmsWorld, genFeatures, worldSeed,
+                        this.nmsWorld.worldProvider.h(), worldConfig);
+                setFinal(cp, generator);
+            } else {
+                this.messages.unknownEnvironment(worldName, environment.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.messages.enabledSuccessfully(worldName);
+    }
+
+    public void setFinal(Field field, Object obj) throws Exception {
+        field.setAccessible(true);
+
+        Field mf = Field.class.getDeclaredField("modifiers");
+        mf.setAccessible(true);
+        mf.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+        field.set(this.nmsWorld.getChunkProviderServer(), obj);
+    }
+}
