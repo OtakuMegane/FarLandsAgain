7634fa8809b2bc01fd6779d028b6407f40bd30d1
OtakuMegane <otakumegane@insaneotaku.com>
2021-06-17 13:57:25 -0400

    Applied Mojang mappings

--- v1_17_R1/src/main/java/com/minefit/xerxestireiron/farlandsagain/v1_17_R1/LoadFarlands.java
+++ v1_17_R1/src/main/java/com/minefit/xerxestireiron/farlandsagain/v1_17_R1/LoadFarlands.java
@@ -10,22 +10,22 @@ import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
 import com.minefit.xerxestireiron.farlandsagain.Messages;
 import com.minefit.xerxestireiron.farlandsagain.ReflectionHelper;
 
-import net.minecraft.server.level.ChunkProviderServer;
-import net.minecraft.server.level.WorldServer;
+import net.minecraft.server.level.ServerChunkCache;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.NoiseSampler;
 import net.minecraft.world.level.levelgen.synth.BlendedNoise;
-import net.minecraft.world.level.levelgen.synth.NoiseGeneratorOctaves;
+import net.minecraft.world.level.levelgen.synth.PerlinNoise;
 
 public class LoadFarlands {
     private final World world;
-    private final WorldServer nmsWorld;
+    private final ServerLevel nmsWorld;
     private final String worldName;
     private String originalGenName;
     private final Messages messages;
     private ChunkGenerator originalGenerator;
     private final ConfigurationSection worldConfig;
-    private ChunkProviderServer chunkServer;
+    private ServerChunkCache serverChunkCache;
     public final ConfigValues configValues;
     private boolean isPaper;
 
@@ -37,10 +37,10 @@ public class LoadFarlands {
         this.nmsWorld = ((CraftWorld) world).getHandle();
         this.messages = new Messages(pluginName);
         this.configValues = new ConfigValues(this.worldName, this.worldConfig, this.isPaper);
-        this.chunkServer = (ChunkProviderServer) this.nmsWorld.getChunkProvider();
-        this.originalGenerator = this.chunkServer.getChunkGenerator();
+        this.serverChunkCache = (ServerChunkCache) this.nmsWorld.getChunkProvider();
+        this.originalGenerator = this.serverChunkCache.getGenerator();
         this.originalGenName = this.originalGenerator.getClass().getSimpleName();
-        overrideGenerator();
+        modifyGenerator();
     }
 
     public void restoreGenerator() {
@@ -49,14 +49,14 @@ public class LoadFarlands {
         }*/
     }
 
-    public void overrideGenerator() {
+    public void modifyGenerator() {
         Environment environment = this.world.getEnvironment();
 
         //NOTE: Flat map type does not use noise generators for terrain so Far Lands can't be done there
         // The equivalent would just be a solid block anyway
         boolean enabled = false;
 
-        if (this.nmsWorld.isFlatWorld()) {
+        if (this.nmsWorld.isFlat()) {
             this.messages.providerFlat(this.worldName);
             return;
         } else if (!isRecognizedGenerator(environment, this.originalGenName)) {
@@ -65,35 +65,26 @@ public class LoadFarlands {
         } else {
             int divisor = (environment == Environment.THE_END) ? 8 : 4;
 
-                /*if (this.isPaper) {
-                    enabled = setGenerator(new FLA_ChunkGeneratorAbstract_Paper(chunkManager, this.nmsWorld.getSeed(),
-                            h, this.configValues, this.originalGenerator, divisor));
-                } else {
-                    enabled = setGenerator(new FLA_ChunkGeneratorAbstract(chunkManager, this.nmsWorld.getSeed(), h,
-                            this.configValues, this.originalGenerator, divisor));
-                }*/
-
-
             try {
-                Field uField = ReflectionHelper.getField(this.originalGenerator.getClass(), "u", true);
-                uField.setAccessible(true);
-                NoiseSampler sampler = (NoiseSampler) uField.get(this.originalGenerator);
+                Field samplerField = ReflectionHelper.getField(this.originalGenerator.getClass(), "u", true);
+                samplerField.setAccessible(true);
+                NoiseSampler sampler = (NoiseSampler) samplerField.get(this.originalGenerator);
 
                 Field blendedNoiseField = ReflectionHelper.getField(sampler.getClass(), "h", true);
                 blendedNoiseField.setAccessible(true);
                 BlendedNoise blendedNoise = (BlendedNoise) blendedNoiseField.get(sampler);
 
-                Field aField = ReflectionHelper.getField(blendedNoise.getClass(), "a", true);
-                aField.setAccessible(true);
-                NoiseGeneratorOctaves minLimitNoise = (NoiseGeneratorOctaves) aField.get(blendedNoise);
+                Field minLimitNoiseField = ReflectionHelper.getField(blendedNoise.getClass(), "a", true);
+                minLimitNoiseField.setAccessible(true);
+                PerlinNoise minLimitNoise = (PerlinNoise) minLimitNoiseField.get(blendedNoise);
 
-                Field bField = ReflectionHelper.getField(blendedNoise.getClass(), "b", true);
-                bField.setAccessible(true);
-                NoiseGeneratorOctaves maxLimitNoise = (NoiseGeneratorOctaves) bField.get(blendedNoise);
+                Field maxLimitNoiseField = ReflectionHelper.getField(blendedNoise.getClass(), "b", true);
+                maxLimitNoiseField.setAccessible(true);
+                PerlinNoise maxLimitNoise = (PerlinNoise) maxLimitNoiseField.get(blendedNoise);
 
-                Field cField = ReflectionHelper.getField(blendedNoise.getClass(), "c", true);
-                cField.setAccessible(true);
-                NoiseGeneratorOctaves mainNoise = (NoiseGeneratorOctaves) cField.get(blendedNoise);
+                Field mainNoiseField = ReflectionHelper.getField(blendedNoise.getClass(), "c", true);
+                mainNoiseField.setAccessible(true);
+                PerlinNoise mainNoise = (PerlinNoise) mainNoiseField.get(blendedNoise);
 
                 FLA_BlendedNoise newBlendedNoise = new FLA_BlendedNoise(minLimitNoise, maxLimitNoise, mainNoise, this.configValues, divisor);
                 ReflectionHelper.setFinal(blendedNoiseField, sampler, newBlendedNoise);
